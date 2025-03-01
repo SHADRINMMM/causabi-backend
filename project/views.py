7fa653b1e13ad4c151eefbcd55b145bf722f4cd1
SHADRINMMM <shadrinMihailDev@gmail.com>
2025-03-01 23:51:46 +0100

    f

--- project/views.py
+++ project/views.py
@@ -3,7 +3,7 @@ import re
 import pandas as pd
 import numpy as np
 
-from flask import Blueprint,  request,jsonify#,render_template, jsonify, make_response, redirect, url_for, stream_with_context,stream_template,Response,send_file,render_template_string
+from flask import Blueprint, json,  request,jsonify#,render_template, jsonify, make_response, redirect, url_for, stream_with_context,stream_template,Response,send_file,render_template_string
 #from flask_login import login_required, current_user, login_user
 from .models import  User
 
@@ -141,6 +141,128 @@ def executor_a_d_vis(result_code: str, h: str, user_id:str ) -> pd.DataFrame:
     except Exception as e:
         # В случае ошибки возвращаем DataFrame с сообщением об ошибке
         return pd.DataFrame({"error": [f"Traceback (most recent call last):\n{str(e)}"]})
+
+def get_tables_info(list_file_name, user_id):
+    """
+    Собирает информацию о таблицах из списка CSV-файлов.
+
+    Параметры:
+    list_file_name (list): Список путей к CSV-файлам.
+
+    Возвращает:
+    str: JSON-строка с информацией о таблицах.
+    """
+    tables_info = []
+
+    for file_name in list_file_name:
+        try:
+            df = pd.read_csv(f"{PATH_TO}/{user_id}/{file_name}")
+
+            if df.empty:
+                table_info = {
+                    "file_name": file_name,
+                    "num_rows": 0,
+                    "num_columns": 0,
+                    "columns": [],
+                    "dtypes": {},
+                    "missing_values": {},
+                    "column_details": {}
+                }
+                tables_info.append(table_info)
+                continue
+
+            table_info = {
+                "file_name": file_name,
+                "num_rows": len(df),
+                "num_columns": len(df.columns),
+                "columns": list(df.columns),
+                "dtypes": df.dtypes.astype(str).to_dict(),
+                "missing_values": df.isnull().sum().to_dict(),
+                "column_details": {}
+            }
+
+            for column in df.columns:
+                col_info = {}
+                dtype = str(df[column].dtype)
+                if 'int' in dtype:
+                    col_info['type'] = 'integer'
+                    col_info['range'] = (int(df[column].min()), int(df[column].max()))
+                    if len(df) > 5:
+                        col_info['examples'] = df[column].dropna().sample(min(5, len(df))).tolist()
+                    else:
+                        col_info['examples'] = df[column].dropna().tolist()
+
+                elif 'datetime' in dtype:
+                    col_info['type'] = 'datetime'
+                    col_info['range'] = (df[column].min().strftime('%Y-%m-%d %H:%M:%S'),
+                                        df[column].max().strftime('%Y-%m-%d %H:%M:%S'))
+                    if len(df) > 5:
+                        col_info['examples'] = df[column].dropna().sample(min(5, len(df))).dt.strftime('%Y-%m-%d %H:%M:%S').tolist()
+                    else:
+                        col_info['examples'] = df[column].dropna().dt.strftime('%Y-%m-%d %H:%M:%S').tolist()
+                elif 'object' in dtype:
+                    col_info['type'] = 'string'
+                    if len(df) > 5:
+                        col_info['examples'] = df[column].dropna().sample(min(5, len(df))).tolist()
+                    else:
+                        col_info['examples'] = df[column].dropna().tolist()
+                else:
+                    print(f"Необработанный тип данных {dtype} для столбца {column}")
+
+                table_info['column_details'][column] = col_info
+
+            tables_info.append(table_info)
+
+        except Exception as e:
+            table_info = {
+                "file_name": file_name,
+                "error": str(e)
+            }
+            tables_info.append(table_info)
+
+
+    tables_info_json = json.dumps(tables_info, indent=4, ensure_ascii=False) 
+    return tables_info_json
+
+
+def get_csv_files(directory):
+    """Gets a list of CSV files in the specified directory."""
+    try:
+        # Use os.listdir to get all files and directories in the specified directory
+        all_files = os.listdir(directory)
+        # Filter the list to include only files ending with .csv
+        csv_files = [f for f in all_files if f.endswith('.csv') and os.path.isfile(os.path.join(directory, f))]
+        return csv_files
+    except FileNotFoundError:
+        return []  # Return empty list if directory doesn't exist
+    except OSError as e:  #Catch general OS errors.
+        print(f"Error reading directory {directory}: {e}")
+        return []
+
+@views.route('/get-tables-info', methods=['POST'])
+def get_info():
+    token = request.headers.get("Authorization")
+
+    
+    # Проверяем токен
+    if token != SECRET_TOKEN:
+        return jsonify({"error": "Access denied. Invalid token."}), 403
+
+    # Получаем данные из запроса
+    data = request.json
+    
+    # Проверяем, что все необходимые параметры переданы
+    if not data or 'result_code' not in data or 'h' not in data:
+        return jsonify({"error": "Missing 'result_code' or 'h' in request"}), 400
+    
+    # Извлекаем параметры
+    user_id = data['user_id']
+    file_list = get_csv_files(f"{PATH_TO}/{user_id}")
+    
+    tables_info_json = get_tables_info(file_list, user_id)
+
+    return jsonify({"result": tables_info_json})
+
 
 
 
